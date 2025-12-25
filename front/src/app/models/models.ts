0a796866f7cd02136cdf6b07ba97d06c7a0f764b
medini <mohameddhia.medini@insat.ucar.tn>
2025-12-25 16:33:27 +0100

    update dat models

--- front/src/app/models/models.ts
+++ front/src/app/models/models.ts
@@ -1,8 +1,15 @@
 export interface Team {
-  team_key: string;
+  team_key: number;
   team_name: string;
   team_logo: string;
   players?: Player[];
+  coaches?: Coach[];
+}
+
+export interface Coach {
+  coach_name: string;
+  coach_country: string | null;
+  coach_age: string | null;
 }
 
 export interface Player {
@@ -19,6 +26,34 @@ export interface Player {
   player_rating: string;
   player_image: string;
   player_country: string | null;
+  player_injured: string;
+  player_is_captain: string;
+  player_saves: string;
+  player_substitute_out: string;
+  player_substitutes_on_bench: string;
+  player_birthdate: string;
+  player_shots_total: string;
+  player_goals_conceded: string;
+  player_fouls_committed: string;
+  player_tackles: string;
+  player_blocks: string;
+  player_crosses_total: string;
+  player_interceptions: string;
+  player_clearances: string;
+  player_dispossesed: string;
+  player_inside_box_saves: string;
+  player_duels_total: string;
+  player_duels_won: string;
+  player_dribble_attempts: string;
+  player_dribble_succ: string;
+  player_pen_comm: string;
+  player_pen_won: string;
+  player_pen_scored: string;
+  player_pen_missed: string;
+  player_passes: string;
+  player_passes_accuracy: string;
+  player_key_passes: string;
+  player_woordworks: string;
 }
 
 export interface Fixture {
@@ -26,11 +61,14 @@ export interface Fixture {
   event_date: string;
   event_time: string;
   event_home_team: string;
-  home_team_key: string;
+  home_team_key: number;
+  home_team_logo: string;
   event_away_team: string;
-  away_team_key: string;
+  away_team_key: number;
+  away_team_logo: string;
   event_final_result: string;
   event_status: string;
+  event_stadium: string;
   league_name: string;
   country_name: string;
 }
