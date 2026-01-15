3ff2e99fbaf8342cb608693e4eeacdb5b032f515
khalil ghimaji <khalil.ghimaji@insat.ucar.tn>
2026-01-15 16:19:27 +0100

    fixing the hydrateFromSnapshot

--- front/src/app/match/services/match-api.service.ts
+++ front/src/app/match/services/match-api.service.ts
@@ -1,11 +1,43 @@
 import {HttpClient} from '@angular/common/http';
 import {inject, Injectable} from '@angular/core';
+import {forkJoin} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class MatchApiService {
   private http = inject(HttpClient);
+  private BASE_URL = 'https://apiv2.allsportsapi.com/football/';
 
   getMatch(matchId: string) {
-    return this.http.get<MatchDto>(`/api/matches/${matchId}`);
+    const generalInfo = this.http.get<MatchDto>(this.BASE_URL,{
+      params : {
+        met:'Fixtures',
+        APIkey:process.env.APIKEY!,
+        matchId: matchId
+      }
+    });
+    const videos = this.http.get<VideoHighlight[]>(this.BASE_URL,{
+      params : {
+        met:'Videos',
+        APIkey:process.env.APIKEY!,
+        eventId: matchId
+      }
+    });
+    const h2h = this.http.get<HeadToHead>(this.BASE_URL,{
+      params : {
+        met:'H2H',
+        APIkey:process.env.APIKEY!,
+        firstTeamId: 'team1',
+        secondTeamId: 'team2'
+      }
+    });
+
+    return forkJoin({
+      generalInfo,
+      highlights: videos,
+      h2h
+    }).pipe(
+      map(({ generalInfo, highlights, h2h }) => ({ ...generalInfo, highlights, h2h }))
+    );
   }
 }
--- front/src/app/match/services/match-resource.factory.ts
+++ front/src/app/match/services/match-resource.factory.ts
@@ -6,6 +6,11 @@ import {concat, tap} from 'rxjs';
 import {filter} from 'rxjs/operators';
 import {MatchStatus, Score} from '../types/match.types';
 import {MatchEvent} from '../types/timeline.types';
+import {MatchHeader} from '../sections/match-header/match-header.section';
+import {Lineups} from '../sections/lineups-pitch/lineups-pitch.section';
+import {TeamStats} from '../types/stats.types';
+import {HeadToHead} from '../types/h2h.types';
+import {VideoHighlight} from '../types/highlight.types';
 
 @Injectable({ providedIn: 'root' })
 export class MatchResourceFactory {
@@ -40,23 +45,88 @@ export class MatchResourceFactory {
 
 function createMatchSignals() {
   return {
-    score: signal<Score | null>(null),
-    time: signal<number | null>(null),
-    status: signal<MatchStatus | null>(null),
-    timeline: signal<MatchEvent[]>([]),
+    matchHeaderSignal: signal<MatchHeader|null>(null),
+    timelineSignal: signal<MatchEvent[]>([]),
+    lineupsSignal: signal<Lineups|null>(null),
+    statsSignal: signal<TeamStats|null>(null),
+    h2hSignal: signal<HeadToHead|null>(null),
+    highlightsSignal: signal<VideoHighlight[]>([]),
   };
 }
 
 type MatchSignals = ReturnType<typeof createMatchSignals>;
 
 function hydrateFromSnapshot(
-  dto: MatchDto,
+  dto: any,
   s: MatchSignals
 ) {
-  s.score.set(dto.score);
-  s.time.set(dto.minute);
-  s.status.set(dto.status);
-  s.timeline.set(dto.timeline);
+  s.matchHeaderSignal.set({
+    status:dto.event_status,
+    homeTeam: dto.event_home_team,
+    awayTeam: dto.event_away_team,
+    score: dto.event_ft_result,
+  });
+  s.timelineSignal.update(t => [...t, dto.goalscorers.map(
+    g => ({
+      type: 'GOAL' as const,
+      minute: g.time,
+      team: g.home_scorer ? 'home' : 'away',
+      player: g.home_scorer || g.away_scorer,
+      detail: g.home_assist ? `Assist: ${g.home_assist}` : g.away_assist ? `Assist: ${g.away_assist}`: '',
+    })
+  )]);
+  s.timelineSignal.update(t => [...t, dto.substitutions.map(
+    s => ({
+      type: 'SUBSTITUTION' as const,
+      minute: s.time,
+      team: s.home_scorer ? 'home' : 'away',
+      player: s.home_scorer ? s.home_scorer.in : s.away_scorer.in,
+      detail: `OUT: ${s.home_scorer ? s.home_scorer.out : s.away_scorer.out}`,
+    })
+  )]);
+  s.timelineSignal.update(t => [...t, dto.cards.map(
+    c => ({
+      type: c.card === 'yellow card' ? 'YELLOW_CARD' as const : 'RED_CARD' as const,
+      minute: c.time,
+      team: c.info,
+      player: c.info == 'home' ? c.home_fault : c.away_fault,
+      detail: '',
+    })
+  )]);
+  s.timelineSignal.update(t=> t.sort((a,b) => a.minute - b.minute));
+  // todo
+  // s.lineupsSignal.set({
+  //
+  // })
+  s.statsSignal.set(dto.statistics.map(s => (
+    {
+      label: s.type,
+      homeValue: s.home,
+      awayValue: s.away,
+    })
+  ));
+  s.h2hSignal.set({
+    homeTeamLogo: dto.home_team_logo,
+    awayTeamLogo: dto.away_team_logo,
+    recentForm: dto.h2h.map(h => {
+      const final_result = h.H2H.event_final_result;
+      let [homeGoals, awayGoals] = final_result.split('-').map(Number);
+      let outcome: 'W' | 'D' | 'L';
+      if (homeGoals > awayGoals) {
+        outcome = 'W';
+      } else if (homeGoals < awayGoals) {
+        outcome = 'L';
+      } else {
+        outcome = 'D';
+      }
+      return outcome;
+    }),
+  })
+  // todo
+  s.highlightsSignal.set(dto.highlights.map(h => ({
+    title: h.video_title,
+    url: h.video_url,
+  })));
 }
 
 function applyEvent(
@@ -64,17 +134,15 @@ function applyEvent(
   s: MatchSignals
 ) {
   switch (event.type) {
-    case 'goal':
+    case 'GOAL':
       s.score.set(event.score);
       s.timeline.update(t => [...t, event]);
       break;
 
-    case 'time':
-      s.time.set(event.minute);
+    case 'YELLOW_CARD':
       break;
 
-    case 'status':
-      s.status.set(event.status);
+    case 'RED_CARD':
       break;
   }
 }
